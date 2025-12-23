dfc2c282c751366fb63f9e7649636c10c4b76eba
hadiaalvi2 <hadiaalvi18@gmail.com>
2025-12-23 16:22:26 +0500

    updates

--- backend/config/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const connectDB = async () => {
-  await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/unievent');
-  console.log("MongoDB connected");
-}
-
-export default connectDB;
--- frontend/lib/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "http://localhost:5000/api", // change the port if your backend uses a different one
-});
-
-export default api;
-
